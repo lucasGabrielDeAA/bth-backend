fe80b17a67a5eff06c6c0b003d611e232424a5d0
lucasGabrielDeAA <lg.technus@gmail.com>
2020-03-25 12:20:24 -0300

    Pagination added to IncidentController.index's endpoint

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -2,7 +2,16 @@ const connection = require('../database');
 
 module.exports = {
   async index(request, response) {
-    const incidents = await connection('incident').select('*');
+    const { page = 1 } = request.query;
+
+    const [count] = await connection('incident').count();
+
+    const incidents = await connection('incident')
+      .limit(5)
+      .offset((page - 1) * 5)
+      .select('*');
+
+    response.header('X-Total-Count', count['count(*)']);
 
     return response.json(incidents);
   },
