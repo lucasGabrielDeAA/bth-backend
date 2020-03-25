7e2dd2e1d34858b004ea61d0dd912f20aeccda6e
lucasGabrielDeAA <lg.technus@gmail.com>
2020-03-25 12:25:30 -0300

    cors lib added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const PORT = 3333;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
 app.use(routes);
 
 app.listen(PORT);
