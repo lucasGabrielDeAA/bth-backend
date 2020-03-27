ca0b0d050d21b42456ec9f563d65f1643caa9d9d
lucasGabrielDeAA <lg.technus@gmail.com>
2020-03-27 12:06:16 -0300

    celebrate's errors added to application's index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const PORT = 3333;
@@ -9,5 +10,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
 app.listen(PORT);
